b09e6f69eaab51a3f12ead081e52aa2e969a5d8b
Spugnam <quentin.picard@gmail.com>
2018-01-02 11:48:03 -0500

    remove quandl_test.py

--- CryptoGoats/quandl_test.py
+++
@@ -1,8 +0,0 @@
-import psycopg2
-
-conn = psycopg2.connect(host="35.185.19.253",
-                 database="crypto",
-                 port="5432",
-                 user="cryptogoats",
-                 password="+tAVMsTH*2\30!fh")
-print(conn)
